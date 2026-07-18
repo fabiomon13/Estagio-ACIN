9151b497a9df0facce51dd96247713bcf421a762
Jackeline Gonçalves <jackeline.camara04@gmail.com>
2026-07-18 20:07:39 +0100

    Configure Prettier and pre-commit hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <>
       <main className="flex min-h-screen items-center justify-center bg-slate-950">
-        <h1 className="text-4xl font-bold text-white">Scan & Serve</h1>
+        <h1 className="text-4xl font-bold text-red-500">Scan & Serve</h1>
       </main>
     </>
   );
