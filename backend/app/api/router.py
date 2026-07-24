875f4b8bc757d17de70aa899580390cc56f34681
Rui Sousa <159160590+rms-13@users.noreply.github.com>
2026-07-24 10:43:47 +0100

    Make guest buffet optional

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -8,3 +8,12 @@ api_router.include_router(
     health_router,
     tags=["Health"],
 )
+api_router.include_router(
+    staff_router
+)
+
+api_router.include_router(
+    client_router,
+    prefix="/client",
+    tags=["Client"],
+)
