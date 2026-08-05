669d55c5cb87918436f913451756498d85641a2c
Rui Sousa <159160590+rms-13@users.noreply.github.com>
2026-08-05 14:53:33 +0100

    set :51474 as default for vite

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
